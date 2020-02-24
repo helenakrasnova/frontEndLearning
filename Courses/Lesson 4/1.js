5a6c4ae621104a6f2e83869bc8278fa0c11bddbe
Helena Krasnova <alenka5354@mail.ru>
2020-02-24 21:35:11 +0300

    Lessons 1-6 uploaded

---
+++ Courses/Lesson 4/1.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let a=1;
+let b=2;
+let c=3;
+const d=4;
+
+// d=8;
+// a=d;
+
+let string='5';
+
+console.log(typeof(a+b+c+d+string));
+console.log(typeof(d));
---
+++ Courses/Lesson 6/2.js
@@ -0,0 +1,53 @@
+// "use srtict"
+
+// let a = 4;
+// let b = 3;
+// let c = (a / b) + (5 / 4);
+// console.log(a / b);
+// console.log(c);
+
+// c = '12';
+
+// console.log(typeof a);
+// console.log(typeof (b));
+
+// console.log(typeof b === typeof c);
+
+// a = Symbol('id');
+// b = Symbol('id');
+
+// console.log(a === b);
+
+// let Lesha = {
+//     name: "Lesha",
+//     age: 36,
+//     jump: function () { return 'ura'; },
+// }
+
+// console.log(typeof Lesha.jump);
+// alert(Lesha.jump());
+
+// console.log(String(1));
+// console.log(String(true));
+// console.log(String('123213'));
+// console.log(String(undefined));
+// console.log(String(null));
+
+// console.log(null + undefined);
+// console.log(+null);
+
+console.log(Boolean(1));
+console.log(Boolean(true));
+console.log(Boolean('123213'));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+
+console.log(Boolean('------------'));
+
+console.log(Boolean(''));
+console.log(Boolean(0));
+console.log(Boolean(false));
+console.log(Boolean({}));
+
+console.log(!!0);
