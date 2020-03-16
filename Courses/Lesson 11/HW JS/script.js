04e90fcb1f15fb1dec255a63d11c9a4caeeb94bc
Helena Krasnova <alenka5354@mail.ru>
2020-03-16 13:07:32 +0300

    Add hw11

---
+++ Courses/Lesson 11/HW JS/script.js
@@ -0,0 +1,10 @@
+let browser = prompt();
+if (browser === 'Edge') {
+    alert("You've got the Edge!");
+}
+if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+    alert('Okay we support these browsers too');
+}
+else {
+    alert('We hope that this page looks ok!');
+}
